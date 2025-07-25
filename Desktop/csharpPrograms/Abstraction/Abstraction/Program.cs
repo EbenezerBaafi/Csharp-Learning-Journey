9959c6818a1a074316d6d8c6bf3c5bc3fcb6d329
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-07-25 03:46:15 +0000

    Abstraction

---
+++ Desktop/csharpPrograms/Abstraction/Abstraction/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Abstraction;
+
+Console.WriteLine("----------------------------");
+Console.WriteLine("------ABSTRACTION-----------");
+
+var animal = new Dog();
+animal.Eat();
+animal.MakeSound();
+
+/*var bird = new Bird();
+bird.Eat();*/
+
+
+
+Console.WriteLine("------Shape examples-----------");
+var square = new Square(11);
+Console.WriteLine($"The area of the square is {square.Area()}\n"+
+                  $"The perimeter of the square is {square.Perimeter()}\n"
+    );
+
+
+var rectangle = new Rectangle(5, 33);
+Console.WriteLine($"The area for this given rectangle is {rectangle.Area()}\n" +
+                  $"The perimeter for the rectangle is {rectangle.Perimeter()}");
---
+++ Desktop/csharpPrograms/Abstraction/Abstraction/Shape.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Shape
+    {
+        public abstract double Area();
+        public abstract double Perimeter();
+
+    }
+    public  class Square : Shape
+    {
+        private double side;
+        public Square(double side)
+        {
+            this.side = side;
+        }
+        public override double Area()
+        {
+            return side;
+        }
+        public override double Perimeter()
+        {
+            return side * 4;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        double length;
+        double breadth;
+
+        public  Rectangle(double length, double breadth)
+        {
+            this.length = length;
+            this.breadth = breadth;
+        }
+        public override double Area()
+        {
+            return length * breadth;
+        }
+
+        public override double Perimeter()
+        {
+            return 2*(length + breadth);
+        }
+    }
+
+}
