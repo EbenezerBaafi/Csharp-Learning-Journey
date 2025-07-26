7707f83edc939475a0edeed4f5734215e319e06a
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-07-26 03:46:53 +0000

    Exceptions

---
+++ source/repos/exception/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ExceptionTut
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeat = 1;
+            int response = 1;
+
+            while (response == 1)
+            {
+
+                try
+                {
+
+                    Console.WriteLine($"Hello {repeat} ");
+                    Console.Write("Repeat ? 1: Yes, 0: No Ans");
+                    response = Convert.ToInt32(Console.ReadLine());
+
+                    if (response != 1 && response != 0)
+                    {
+                        throw new ValidInputException();
+                    }
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"*****FormatException Occured ********\n");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exception details -> {e.ToString()} ");
+                }
+
+                finally
+                {
+                    repeat++;
+
+                }
+            }
+        }
+    }
+}
